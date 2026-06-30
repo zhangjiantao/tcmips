28725cfbbfa7b9f21191940959b9a9eadbe4ba76
zhangjiantao <bitt.z@qq.com>
2026-06-30 22:54:26 +0800

    game save for v2.1

---
+++ gamesave/schematics/symphony_12_budget/Default/new_program.asm
@@ -0,0 +1,1168 @@
+keyboard r1
+or r1, zr, 3
+screen r1, 5
+time_0 r2
+time_1 r3
+counter r3
+in r6
+in r7
+in r8
+in r9
+in r10
+in r11
+in r12
+in r13
+in sp
+in flags
+
+in r1
+in r2
+in r3
+in r4
+in r5
+in r6
+in r7
+in r8
+in r9
+in r10
+in r11
+in r12
+in r13
+in sp
+in flags
+
+add r5, r5, r12
+nand r2, r10, r12
+or sp, flags, r2
+out r3
+add zr, r3, 59245
+in r7
+or r10, r10, r10
+label_8:
+in r5
+in sp
+cmp r5, sp
+jg label_8
+lsr r13, r5, 31616
+in r5
+add flags, sp, r5
+add r5, flags, 65077
+jg label_14
+sub sp, r2, 64397
+label_14:
+out r9
+xor r7, zr, r13
+sub zr, r5, 59653
+sub r3, r10, r13
+jle label_21
+in r6
+label_21:
+lsl r7, sp, r3
+xor flags, flags, 56755
+and sp, r9, r5
+lsl r8, r4, r13
+add zr, flags, 32398
+out r3
+lsl r3, r12, r8
+and r6, r10, r1
+or r3, r3, 40882
+add r9, r9, 21023
+jmp label_34
+sub r11, r1, 59007
+label_34:
+nand r12, r8, 246
+jmp label_38
+jge label_39
+out r9
+label_39:
+label_38:
+xor r8, zr, r13
+jle label_44
+jmp label_45
+add r4, flags, 48509
+label_45:
+label_44:
+jmp label_49
+nor r12, r9, 58110
+label_49:
+jae label_52
+nand r7, r4, 49175
+label_52:
+jle label_55
+nand sp, r5, r6
+label_55:
+or r12, zr, 14630
+jb label_59
+nand r12, r1, 61817
+label_59:
+in r3
+cmp zr, r8
+jmp label_64
+cmp r10, r2
+label_64:
+nand r6, r2, r12
+or flags, r3, 36738
+xor r10, r4, sp
+lsl r11, r7, 16359
+jg label_71
+lsl r13, r13, 16050
+label_71:
+nor r10, r8, 19776
+out r4
+nor r11, r5, r3
+nor r2, flags, 60045
+jmp label_78
+out r13
+label_78:
+jge label_81
+or r8, r9, 50129
+label_81:
+je label_84
+lsr zr, r1, 64939
+label_84:
+jmp label_87
+xor r12, r10, 43706
+label_87:
+je label_90
+jb label_91
+jbe label_92
+or r9, sp, r11
+label_92:
+label_91:
+label_90:
+add r10, r3, r10
+sub r1, r6, 62120
+and r11, r7, 12015
+and r12, r1, 39857
+jl label_101
+jmp label_102
+add zr, r1, 41554
+label_102:
+label_101:
+sub r9, r13, 8550
+ja label_107
+nand r12, r5, 7924
+label_107:
+and r2, flags, 5672
+lsr r12, r2, r13
+xor r11, r9, r10
+lsr r9, r3, r13
+sub r8, r4, r10
+sub r9, zr, 20945
+in zr
+cmp r8, r4
+cmp r12, r7
+jl label_119
+jge label_120
+jg label_121
+label_122:
+in r10
+in r1
+cmp r10, r1
+jmp label_122
+label_121:
+label_120:
+label_119:
+cmp r12, r7
+cmp r4, r8
+cmp sp, r2
+cmp r1, r11
+sub r4, r11, r2
+sub r1, r3, 64474
+jb label_133
+sub r9, r1, r9
+label_133:
+sub r7, r10, r6
+cmp r2, r5
+label_138:
+in r1
+in r3
+cmp r1, r3
+jge label_138
+cmp r3, flags
+or r4, r11, 24993
+label_142:
+in r2
+in r13
+cmp r2, r13
+jge label_142
+lsr r2, r13, 29459
+jne label_145
+jg label_146
+je label_147
+jmp label_148
+xor r2, r9, r11
+label_148:
+label_147:
+label_146:
+label_145:
+jmp label_154
+cmp r8, r11
+label_154:
+add r3, r7, sp
+sub sp, r2, r4
+cmp r13, r13
+add r1, flags, r6
+jne label_161
+add r12, r13, 52775
+label_161:
+sub sp, r7, 20418
+nor r7, r13, 9876
+label_166:
+in r5
+in r7
+cmp r5, r7
+jae label_166
+jae label_168
+jmp label_169
+nor r1, sp, r5
+label_169:
+label_168:
+add r1, zr, r1
+in r4
+jmp label_175
+in r3
+label_175:
+nor zr, r4, 12217
+sub sp, r13, 64799
+jmp label_180
+or r12, zr, 60162
+label_180:
+lsr r5, r9, r4
+xor r8, r11, r7
+nor r2, r12, 945
+and r3, r6, 6276
+sub r3, r5, 55574
+add r1, r9, 15155
+lsr r1, flags, 60002
+jne label_190
+nor flags, r1, r4
+label_190:
+in r7
+jmp label_194
+ja label_195
+cmp r8, r9
+label_195:
+label_194:
+nand r1, r12, 61177
+xor zr, r12, 53793
+xor sp, r3, 56292
+label_202:
+in sp
+in r12
+cmp sp, r12
+jl label_202
+cmp r9, r11
+jmp label_205
+lsr r3, r10, r9
+label_205:
+or r8, r4, r9
+lsr r5, r3, 31435
+jmp label_210
+jae label_211
+or flags, r5, r7
+label_211:
+label_210:
+jbe label_215
+xor r10, r5, r9
+label_215:
+jge label_218
+jb label_219
+lsl flags, sp, 1990
+label_219:
+label_218:
+label_223:
+in r10
+in r12
+cmp r10, r12
+jg label_223
+jmp label_225
+add r10, r13, r7
+label_225:
+sub r3, r4, 20332
+add r1, r13, 8225
+lsr r6, r12, r2
+nor r10, r13, r12
+lsr r9, r2, r3
+lsr sp, r2, r5
+add r2, r9, r13
+in r2
+jne label_236
+jge label_237
+jg label_238
+jmp label_239
+jae label_240
+lsr r11, r5, r1
+label_240:
+label_239:
+label_238:
+label_237:
+label_236:
+add sp, r6, 26176
+lsl r10, r13, 7150
+label_249:
+in r3
+in r7
+cmp r3, r7
+je label_249
+nor r2, r3, r3
+label_252:
+in r8
+in r2
+cmp r8, r2
+jle label_252
+jmp label_254
+lsr r6, r1, r5
+label_254:
+jl label_257
+add r9, r3, r9
+label_257:
+lsl r10, r12, 7642
+in flags
+add zr, r4, r12
+label_263:
+in r8
+in r6
+cmp r8, r6
+jbe label_263
+label_265:
+in r10
+in r7
+cmp r10, r7
+jg label_265
+nor r2, r6, r8
+jmp label_268
+xor zr, r8, 64707
+label_268:
+nand r4, flags, r3
+jmp label_272
+or r11, r7, r5
+label_272:
+cmp r2, r10
+and r11, r12, r10
+add sp, r5, r10
+add sp, r3, 29296
+add r7, r1, r9
+jmp label_280
+out r13
+label_280:
+jmp label_283
+jle label_284
+sub r8, r13, r11
+label_284:
+label_283:
+lsr r6, flags, r4
+and zr, r8, r5
+add r4, r13, r9
+out flags
+cmp sp, r4
+xor r3, r10, flags
+cmp r2, zr
+and r12, r12, 42093
+lsl r3, r8, r10
+and sp, r6, 60467
+nand r12, r7, r6
+or r10, r9, 23631
+jmp label_300
+cmp r6, r13
+label_300:
+add r2, sp, r10
+jle label_304
+nor r4, r12, 62469
+label_304:
+jb label_307
+lsr r5, r3, 42689
+label_307:
+je label_310
+lsr r5, sp, 11516
+label_310:
+add r12, r4, r6
+jmp label_314
+in r3
+label_314:
+lsl r3, r11, r12
+jmp label_318
+and zr, flags, 47353
+label_318:
+cmp r1, sp
+nand zr, zr, r11
+nand r11, r6, r8
+nand sp, r13, r6
+sub r11, zr, 29038
+sub r4, r5, 53149
+jmp label_327
+add r11, r12, r8
+label_327:
+lsr r10, sp, r5
+add r9, r7, 14803
+add flags, r10, 60857
+cmp sp, zr
+jmp label_334
+add flags, r4, r1
+label_334:
+lsr r1, r6, r5
+label_338:
+in r11
+in r8
+cmp r11, r8
+jl label_338
+jmp label_340
+lsl r6, r2, 64077
+label_340:
+jmp label_343
+or r7, r2, 64128
+label_343:
+nor r1, r4, r8
+jmp label_347
+cmp r2, r4
+label_347:
+and r6, sp, 43872
+jmp label_351
+and sp, r5, r12
+label_351:
+in r9
+xor r12, r3, 60178
+and r7, r9, r5
+label_357:
+in r10
+in r12
+cmp r10, r12
+jl label_357
+sub r12, r3, flags
+in r12
+nand r5, r6, r5
+add r3, r4, 55289
+lsl r13, r7, r10
+jmp label_364
+lsl r11, r6, 50202
+label_364:
+jae label_367
+nand r6, r6, 63849
+label_367:
+xor flags, r11, 1822
+jmp label_371
+ja label_372
+jl label_373
+out r8
+label_373:
+label_372:
+label_371:
+label_378:
+in r10
+in r8
+cmp r10, r8
+jg label_378
+jmp label_380
+jl label_381
+and flags, r4, flags
+label_381:
+label_380:
+jmp label_385
+lsl r12, r6, r8
+label_385:
+nand r3, r9, 59079
+add r10, r3, sp
+out r5
+add r6, sp, 1864
+or r5, r1, flags
+add flags, r1, r4
+add r10, r2, 48428
+label_395:
+in r5
+in r2
+cmp r5, r2
+jne label_395
+jge label_397
+or r4, r9, 37414
+label_397:
+nor r13, r1, 41569
+nand r10, r4, r2
+xor r9, r1, 58782
+and flags, r3, r1
+sub r3, r8, 26098
+jmp label_405
+and r7, r2, r7
+label_405:
+lsr r11, sp, 17285
+lsr r12, sp, r2
+cmp r2, sp
+jbe label_411
+and r1, r1, 35909
+label_411:
+xor r3, r9, 61316
+jg label_415
+nor r7, r7, r7
+label_415:
+add zr, r2, r4
+sub r3, r4, r2
+xor r12, zr, 50599
+jbe label_421
+lsr r6, zr, 51723
+label_421:
+nor sp, r2, 5429
+and r13, r12, 49359
+je label_426
+add r1, r12, r7
+label_426:
+nor r10, r4, 12195
+lsr sp, r6, 64540
+and r11, r7, 25452
+jmp label_432
+cmp flags, sp
+label_432:
+jmp label_435
+add r11, sp, 18841
+label_435:
+label_438:
+in r3
+in r6
+cmp r3, r6
+jne label_438
+add r2, r1, 50706
+cmp r4, r12
+cmp r1, flags
+add flags, sp, r4
+out r5
+jmp label_445
+in r8
+label_445:
+jle label_448
+label_449:
+in r12
+in r3
+cmp r12, r3
+jg label_449
+label_448:
+out r12
+add r10, r7, 39695
+out r4
+sub r4, r5, 43005
+jle label_456
+out r8
+label_456:
+and r11, r4, 33350
+add r2, r9, r7
+label_461:
+in r1
+in r2
+cmp r1, r2
+je label_461
+xor r3, r10, r2
+add r4, r7, r1
+in r4
+sub r2, r13, 12900
+add flags, r8, 32244
+lsl r2, flags, sp
+add r10, flags, r10
+cmp r13, r10
+jne label_471
+label_472:
+in r7
+in r12
+cmp r7, r12
+jae label_472
+label_471:
+jmp label_475
+jne label_476
+jmp label_477
+nand r4, r7, 14313
+label_477:
+label_476:
+label_475:
+jae label_482
+lsr r3, r7, r13
+label_482:
+add r2, r7, r9
+in r4
+lsl r6, sp, r10
+or r10, r9, flags
+label_489:
+in r5
+in r2
+cmp r5, r2
+jle label_489
+nand r4, r5, 64630
+jle label_492
+lsr r6, r5, r9
+label_492:
+lsl flags, r7, r10
+je label_496
+and zr, r13, 6292
+label_496:
+add r8, r10, r11
+lsl r11, r7, 48051
+label_501:
+in r9
+in r6
+cmp r9, r6
+jle label_501
+jb label_503
+xor r9, r9, 33156
+label_503:
+jmp label_506
+nor r8, r11, r13
+label_506:
+add r12, r12, r11
+nand r9, r6, 64621
+cmp r3, zr
+out flags
+label_513:
+in r4
+in sp
+cmp r4, sp
+jl label_513
+and flags, r5, 18745
+nand r10, r12, 20897
+xor r7, r9, 24346
+sub r2, r6, 15260
+and r11, r12, r4
+ja label_520
+and r11, flags, r12
+label_520:
+lsr r10, flags, 10857
+add r9, r5, r10
+cmp r3, sp
+and r8, sp, r8
+label_527:
+in r10
+in r3
+cmp r10, r3
+jne label_527
+or r11, r12, 14148
+jmp label_530
+lsl r13, r9, r11
+label_530:
+label_533:
+in r12
+in r9
+cmp r12, r9
+je label_533
+nand r5, r7, r9
+add r3, r7, sp
+or sp, r8, 22221
+lsr r12, r6, r8
+lsr r13, r5, r12
+cmp r7, r7
+ja label_541
+or r4, r10, 62534
+label_541:
+or r13, r12, zr
+ja label_545
+sub zr, r1, 2705
+label_545:
+jmp label_548
+label_549:
+in r1
+in r7
+cmp r1, r7
+jle label_549
+label_548:
+jmp label_552
+add r3, zr, 13765
+label_552:
+nor r5, r13, r11
+jmp label_556
+label_557:
+in r6
+in sp
+cmp r6, sp
+jg label_557
+label_556:
+out r2
+or r2, r3, r1
+or r12, r6, 58623
+jne label_563
+add r3, r7, 19047
+label_563:
+xor r5, r10, r11
+or r4, zr, 39983
+lsr sp, r7, r1
+or r1, r8, r7
+jb label_570
+add zr, r13, 50768
+label_570:
+jle label_573
+xor r12, r1, 21424
+label_573:
+jmp label_576
+lsr r3, r9, 27505
+label_576:
+xor r13, r12, r5
+in r8
+lsl r12, r6, sp
+jbe label_582
+nor sp, r3, r3
+label_582:
+nand flags, r13, r12
+jg label_586
+xor r12, r6, r8
+label_586:
+add r5, sp, 11102
+nand sp, zr, r11
+lsl r3, r7, 35329
+sub r11, r2, 62490
+or r13, r4, 62496
+add sp, r12, r1
+lsr r12, r9, r7
+jmp label_596
+or r5, r8, r11
+label_596:
+label_599:
+in r11
+in r1
+cmp r11, r1
+jg label_599
+jge label_601
+sub r13, r1, 25210
+label_601:
+jb label_604
+cmp r12, r7
+label_604:
+out r7
+lsl flags, r8, 25201
+add zr, r4, flags
+sub zr, zr, r1
+and r1, r9, r13
+nand r3, r8, 29986
+label_613:
+in r2
+in r7
+cmp r2, r7
+jae label_613
+jb label_615
+or flags, r7, 55270
+label_615:
+nor r10, r12, 17288
+lsr sp, r7, flags
+lsr r5, zr, r5
+cmp r7, sp
+label_622:
+in sp
+in r6
+cmp sp, r6
+jb label_622
+or r10, r3, 51851
+lsl r5, zr, 21077
+out r8
+sub flags, zr, r11
+nor sp, r4, r2
+or sp, r6, r1
+add r9, r12, 11519
+nand r7, r4, r1
+nand flags, r3, 33902
+out r6
+jg label_634
+lsr r8, r1, 50633
+label_634:
+out r11
+jae label_638
+cmp r4, r12
+label_638:
+jmp label_641
+and flags, r1, 45199
+label_641:
+je label_644
+nor zr, flags, sp
+label_644:
+jb label_647
+nor r11, r7, 20471
+label_647:
+nand r5, flags, r4
+nor r6, r8, 23381
+cmp zr, r13
+nor r5, r6, 8184
+jge label_654
+and r3, r2, 41997
+label_654:
+jg label_657
+label_658:
+in r6
+in r2
+cmp r6, r2
+jle label_658
+label_657:
+lsl sp, r3, 39129
+jbe label_662
+jmp label_663
+out r13
+label_663:
+label_662:
+label_667:
+in r12
+in r3
+cmp r12, r3
+jb label_667
+lsl r9, r8, r11
+jge label_670
+sub r2, sp, r3
+label_670:
+add r1, r11, 25488
+cmp r5, r6
+add r3, r7, 38623
+nor r12, r9, 46883
+or r8, flags, 54316
+nor r8, r12, r5
+lsl r2, r2, r4
+add r2, r6, flags
+or r13, r13, zr
+lsl r11, r2, r1
+or zr, r7, 52942
+add r3, r1, flags
+nand r8, r11, r2
+sub sp, r11, r13
+jne label_687
+nand r6, r2, 31045
+label_687:
+jg label_690
+and r7, r5, zr
+label_690:
+xor r6, flags, 61000
+jle label_694
+jl label_695
+label_696:
+in r10
+in r6
+cmp r10, r6
+jbe label_696
+label_695:
+label_694:
+cmp r11, r11
+lsl r1, r8, 54593
+lsr r3, r6, sp
+add r11, r11, 30010
+or r3, r13, 20193
+nand r4, r10, 6899
+add r11, r12, r3
+nand r13, r12, r10
+and r11, r3, r7
+jmp label_709
+xor r11, r8, 40826
+label_709:
+jmp label_712
+cmp sp, r8
+label_712:
+add r12, r8, r12
+nand r3, r12, r9
+sub r7, r6, r6
+add r4, zr, 33177
+lsl r13, r10, r10
+cmp sp, r11
+add r7, zr, r7
+xor r8, sp, r2
+cmp r4, sp
+lsr flags, r9, r11
+jmp label_725
+nor r8, r2, 11137
+label_725:
+add r3, r1, r12
+add r5, r2, 182
+jae label_730
+ja label_731
+jmp label_732
+xor flags, zr, 43399
+label_732:
+label_731:
+label_730:
+je label_737
+label_738:
+in r13
+in r12
+cmp r13, r12
+jbe label_738
+label_737:
+nand zr, r5, 38694
+lsl r11, r7, 18011
+add r11, flags, 17232
+or r8, r12, r1
+jge label_745
+jle label_746
+and r2, r7, r6
+label_746:
+label_745:
+nor r2, r9, 12074
+nand r1, r6, r8
+jmp label_752
+xor r13, r1, 31419
+label_752:
+or r6, r5, 36093
+nor r11, r9, 14684
+nand r6, r5, 21478
+or r1, r4, 57454
+jle label_759
+sub r6, r4, 18858
+label_759:
+jl label_762
+cmp r3, r4
+label_762:
+cmp r10, r6
+jl label_766
+label_767:
+in r1
+in r7
+cmp r1, r7
+jb label_767
+label_766:
+nor r4, r10, 8724
+jne label_771
+and r2, r10, 46839
+label_771:
+add r12, r13, 52739
+jbe label_775
+nor r3, zr, r10
+label_775:
+add r1, zr, 32487
+or r11, sp, 5059
+out r2
+add r4, r2, 12961
+sub r13, r12, r7
+lsr r7, r13, 31482
+jg label_784
+xor sp, r3, r12
+label_784:
+jmp label_787
+sub r11, r4, r3
+label_787:
+xor r3, r2, zr
+label_791:
+in r5
+in r7
+cmp r5, r7
+jae label_791
+nor r4, r10, r11
+label_794:
+in r8
+in r3
+cmp r8, r3
+jle label_794
+add sp, r8, 16916
+or sp, sp, 20128
+or r12, r2, r6
+label_799:
+in r2
+in r1
+cmp r2, r1
+jb label_799
+nor r5, r8, r1
+sub r5, r13, 53801
+add r5, r5, 45524
+cmp r10, r5
+xor r3, r2, r6
+nand r12, r1, 49369
+jbe label_807
+jl label_808
+in r13
+label_808:
+label_807:
+xor r8, r9, 16727
+jbe label_813
+lsr r5, r4, sp
+label_813:
+add flags, flags, 43788
+lsr r9, flags, r13
+ja label_818
+or r3, r2, 3942
+label_818:
+nor r9, r2, 48440
+add r11, r13, r10
+cmp sp, r2
+in r6
+sub r12, r4, 22339
+jne label_826
+or r1, r7, r3
+label_826:
+jmp label_829
+cmp r3, r9
+label_829:
+out r6
+xor r11, r13, r9
+add r4, r12, r5
+jge label_835
+in r11
+label_835:
+out sp
+jb label_839
+xor flags, zr, 59857
+label_839:
+jae label_842
+out zr
+label_842:
+add r4, r6, 16182
+add r1, r12, 41052
+cmp r6, r11
+nand r11, r1, r5
+out r4
+or r6, r10, flags
+sub r9, r10, r12
+or r13, r11, r6
+nor r5, r1, r10
+lsr r9, r4, 36204
+xor r13, zr, r1
+out r2
+add r10, r7, r5
+add r7, r11, r13
+or r10, r11, r4
+and r13, r6, 42659
+add r12, r8, r3
+add r11, r12, r11
+in r5
+lsr r9, r9, 23633
+jmp label_865
+sub r13, r10, r8
+label_865:
+lsr r11, r1, 28749
+lsl sp, r2, r2
+sub r5, r4, 18140
+sub r4, r12, 60895
+nand r11, sp, 47188
+cmp r4, r7
+lsr r3, r3, 56773
+jmp label_875
+lsr r12, r5, 34263
+label_875:
+add r12, r13, 44654
+nor r12, r10, 27672
+add r11, r13, 52639
+nor r12, r4, 24870
+add r13, r13, 18677
+and r2, r4, r9
+ja label_884
+or r12, r8, 15
+label_884:
+lsl r13, r7, r4
+lsl r1, sp, 11630
+or r12, r1, r1
+sub zr, r12, r3
+and flags, r11, r9
+jg label_892
+sub r6, sp, flags
+label_892:
+cmp r9, r9
+lsr sp, r3, 13739
+lsr r3, r7, r4
+add r6, flags, r13
+lsl r8, r9, 60964
+add r13, zr, 2835
+add r12, r9, 35712
+lsr r13, zr, r8
+jmp label_903
+out r3
+label_903:
+jmp label_906
+in r13
+label_906:
+lsr r12, r3, 60596
+add r12, r7, 206
+add r8, r3, 53253
+label_912:
+in r2
+in r5
+cmp r2, r5
+jg label_912
+lsr r8, r9, 11704
+out r8
+lsr zr, r6, r7
+jge label_917
+jmp label_918
+nand r13, r1, r7
+label_918:
+label_917:
+add r10, zr, r3
+or r3, r5, r5
+jl label_924
+lsr r10, r9, 14580
+label_924:
+label_927:
+in r7
+in r10
+cmp r7, r10
+jb label_927
+lsl r4, r8, r1
+add r4, flags, r7
+lsr r9, r2, r7
+nand flags, r4, 8746
+sub flags, r6, r12
+nor r2, r6, sp
+lsl flags, sp, 11055
+in r7
+jmp label_937
+lsr r1, r12, 38391
+label_937:
+add sp, flags, r8
+cmp r11, r1
+xor r8, r9, 5236
+label_943:
+in r9
+in r13
+cmp r9, r13
+jge label_943
+jmp label_945
+out r7
+label_945:
+jle label_948
+lsl r4, r5, 21628
+label_948:
+cmp r13, sp
+xor sp, r9, 13612
+jmp label_953
+add r13, r11, 58944
+label_953:
+jae label_956
+sub r5, r3, r8
+label_956:
+lsl r1, r11, 9161
+in sp
+in r11
+label_962:
+in r1
+in r10
+cmp r1, r10
+jae label_962
+sub r1, r5, 35387
+in r10
+sub r3, r13, r10
+label_967:
+in r8
+in r6
+cmp r8, r6
+jge label_967
+lsr r4, r11, zr
+out r7
+add flags, r5, 43128
+label_972:
+in r13
+in r12
+cmp r13, r12
+jle label_972
+in r5
+add r4, r13, 47957
+jmp label_976
+add r2, r2, 4308
+label_976:
+in r8
+add sp, r1, 52174
+and sp, r6, zr
+nor r4, sp, 1104
+xor zr, r6, 54865
+label_984:
+in r2
+in r11
+cmp r2, r11
+jle label_984
+nor r7, zr, 17962
+lsr zr, flags, 15816
+label_988:
+in r6
+in r5
+cmp r6, r5
+jae label_988
+add zr, r1, r7
+jbe label_991
+and r13, zr, 27376
+label_991:
+add r7, r12, r10
+in zr
+sub flags, r8, 29078
+jb label_997
+or r7, r7, 2961
+label_997:
+lsl r2, r3, r13
